147906708ae8cf5bc1f33486e7a2cce93d45e525
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2025-05-24 21:46:01 +0600

    inserted species and sightings table data

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -3,8 +3,28 @@ CREATE DATABASE conservation_db;
 CREATE TABLE rangers (
     ranger_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    area VARCHAR(100) NOT NULL
+    region VARCHAR(100) NOT NULL
 );
+
+INSERT INTO
+    rangers (name, region)
+VALUES (
+        'Alice Green',
+        'Northern Hills'
+    ),
+    ('Bob White', 'River Delta'),
+    (
+        'Carol King',
+        'Mountain Range'
+    ),
+    (
+        'Meera Sharma',
+        'Eastern Highlands'
+    ),
+    (
+        'Ravi Kumar',
+        'Northern Plains'
+    );
 
 CREATE TABLE species (
     species_id SERIAL PRIMARY KEY,
@@ -13,6 +33,44 @@ CREATE TABLE species (
     discovery_date DATE NOT NULL,
     conservation_status VARCHAR(50) NOT NULL
 );
+
+INSERT INTO
+    species (
+        common_name,
+        scientific_name,
+        discovery_date,
+        conservation_status
+    )
+VALUES (
+        'Snow Leopard',
+        'Panthera uncia',
+        '1775-01-01',
+        'Endangered'
+    ),
+    (
+        'Bengal Tiger',
+        'Panthera tigris tigris',
+        '1758-01-01',
+        'Endangered'
+    ),
+    (
+        'Red Panda',
+        'Ailurus fulgens',
+        '1825-01-01',
+        'Vulnerable'
+    ),
+    (
+        'Asiatic Elephant',
+        'Elephas maximus indicus',
+        '1758-01-01',
+        'Endangered'
+    ),
+    (
+        'Shadow Leopard',
+        'Panthera obscura',
+        '2001-01-01',
+        'Critically Endangered'
+    );
 
 CREATE TABLE sightings (
     sighting_id SERIAL PRIMARY KEY,
@@ -24,3 +82,49 @@ CREATE TABLE sightings (
     FOREIGN KEY (ranger_id) REFERENCES rangers (ranger_id),
     FOREIGN KEY (species_id) REFERENCES species (species_id)
 );
+
+INSERT INTO
+    sightings (
+        species_id,
+        ranger_id,
+        location,
+        sighting_time,
+        notes
+    )
+VALUES (
+        1,
+        1,
+        'Peak Ridge',
+        '2024-05-10 07:45:00',
+        'Camera trap image captured'
+    ),
+    (
+        2,
+        2,
+        'Bankwood Area',
+        '2024-05-12 16:20:00',
+        'Juvenile seen'
+    ),
+    (
+        3,
+        3,
+        'Bamboo Grove East',
+        '2024-05-15 09:10:00',
+        'Feeding observed'
+    ),
+    (
+        1,
+        2,
+        'Snowfall Pass',
+        '2024-05-18 18:30:00',
+        NULL
+    ),
+    (
+        5,
+        4,
+        'Shadow Glen',
+        '2024-05-20 05:15:00',
+        'Unconfirmed shadowy figure seen'
+    );
+
+SELECT * from species;
