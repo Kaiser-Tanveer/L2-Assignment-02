6d89db8c1a29248b9c6586237673230bcb65bc3f
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2025-05-25 08:20:58 +0600

    Problem 8 sooved

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -83,6 +83,50 @@ CREATE TABLE sightings (
     FOREIGN KEY (species_id) REFERENCES species (species_id)
 );
 
+INSERT INTO
+    sightings (
+        species_id,
+        ranger_id,
+        location,
+        sighting_time,
+        notes
+    )
+VALUES (
+        1,
+        1,
+        'Peak Ridge',
+        '2024-05-10 07:45:00',
+        'Camera trap image captured'
+    ),
+    (
+        2,
+        2,
+        'Bankwood Area',
+        '2024-05-12 16:20:00',
+        'Juvenile seen'
+    ),
+    (
+        3,
+        3,
+        'Bamboo Grove East',
+        '2024-05-15 09:10:00',
+        'Feeding observed'
+    ),
+    (
+        1,
+        2,
+        'Snowfall Pass',
+        '2024-05-18 18:30:00',
+        NULL
+    ),
+    (
+        5,
+        4,
+        'Shadow Glen',
+        '2024-05-20 05:15:00',
+        'Unconfirmed shadowy figure seen'
+    );
+
 ---------------------- Problem-01 --------------------------
 INSERT INTO
     rangers (name, region)
@@ -127,48 +171,5 @@ WHERE
 
 ---------------------- Problem-08 --------------------------
 ---------------------- Problem-09 --------------------------
-INSERT INTO
-    sightings (
-        species_id,
-        ranger_id,
-        location,
-        sighting_time,
-        notes
-    )
-VALUES (
-        1,
-        1,
-        'Peak Ridge',
-        '2024-05-10 07:45:00',
-        'Camera trap image captured'
-    ),
-    (
-        2,
-        2,
-        'Bankwood Area',
-        '2024-05-12 16:20:00',
-        'Juvenile seen'
-    ),
-    (
-        3,
-        3,
-        'Bamboo Grove East',
-        '2024-05-15 09:10:00',
-        'Feeding observed'
-    ),
-    (
-        1,
-        2,
-        'Snowfall Pass',
-        '2024-05-18 18:30:00',
-        NULL
-    ),
-    (
-        5,
-        4,
-        'Shadow Glen',
-        '2024-05-20 05:15:00',
-        'Unconfirmed shadowy figure seen'
-    );
 
 SELECT * from sightings;
